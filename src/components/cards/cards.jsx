1bab0ddc9319662401948a1a2ecc6ea6f589a7c1
tech-in-enterprise <tech-in_suport@hotmail.com>
2025-06-05 22:00:13 -0300

    add textfild de comments

--- src/components/cards/cards.jsx
+++ src/components/cards/cards.jsx
@@ -1,20 +1,12 @@
 import React, { useState, useEffect } from 'react'
 import { supabase } from '../../services/supabase'
-import { Box, Link } from '@mui/material'
-import Card from '@mui/material/Card'
-import CardActions from '@mui/material/CardActions'
-import CardContent from '@mui/material/CardContent'
-import Button from '@mui/material/Button'
-import Typography from '@mui/material/Typography'
+import { Box, Grid, Card, CardActions, CardContent, Button, Typography } from '@mui/material'
 import { FaRegStar, FaStar } from "react-icons/fa"
 import { IoChatbubbleEllipsesOutline } from "react-icons/io5"
 import { useDispatch, useSelector } from 'react-redux'
 import { setModalOpen } from '../../redux/authSlice'
 import { favoriteProject, fetchFavorites } from '../../redux/favoritesSlice'
 import { fetchProjects } from '../../redux/projectsSlice'
-import { FaGithub } from "react-icons/fa"
-import HighlightOffIcon from '@mui/icons-material/HighlightOff'
-import TextField from '@mui/material/TextField'
 import AboutProject from './about-project'
 import ComentsAboutProject from './coments-about-project'
 
@@ -85,74 +77,78 @@ export default function ProjectPosterCard() {
   const handleCloseComments = () => setShowComments(false)
 
   return (
-    <React.Fragment>
-      {projects.map((project) => (
-        <Card key={project.id} sx={{ maxWidth: 400, border: '1px solid #ccc', borderRadius: 3, boxShadow: '0 8px 16px rgba(0, 0, 0, 0.2)', backgroundColor: '#FFFFFF', overflow: 'hidden', position: 'relative' }}>
-          {showComments ? (
-           <ComentsAboutProject handleCloseComments={handleCloseComments}/>
-          ) : showText ? (
-            <AboutProject handleResetInfoClick={handleResetInfoClick} />
-          ) : (
-            <div style={{ height: 500, display: 'flex', justifyContent: 'center', alignItems: 'center', overflow: 'hidden' }}>
-              {project.video_url ? (
-                <video autoPlay muted loop width="100%" height="100%" style={{ objectFit: 'cover' }}>
-                  <source src={project.video_url} type="video/mp4" />
-                  Seu navegador não suporta o elemento de vídeo.
-                </video>
+    <Box sx={{ padding: 2 }}>
+      <Grid container spacing={2}>
+        {projects.map((project) => (
+          <Grid item xs={12} sm={6} md={4} key={project.id}>
+            <Card key={project.id} sx={{ maxWidth: 400, border: '1px solid #ccc', borderRadius: 3, boxShadow: '0 8px 16px rgba(0, 0, 0, 0.2)', backgroundColor: '#FFFFFF', overflow: 'hidden', position: 'relative' }}>
+              {showComments ? (
+                <ComentsAboutProject handleCloseComments={handleCloseComments} />
+              ) : showText ? (
+                <AboutProject handleResetInfoClick={handleResetInfoClick} />
               ) : (
-                <Typography sx={{ color: '#000000' }}>Carregando vídeo...</Typography>
+                <div style={{ height: 500, display: 'flex', justifyContent: 'center', alignItems: 'center', overflow: 'hidden' }}>
+                  {project.video_url ? (
+                    <video autoPlay muted loop width="100%" height="100%" style={{ objectFit: 'cover' }}>
+                      <source src={project.video_url} type="video/mp4" />
+                      Seu navegador não suporta o elemento de vídeo.
+                    </video>
+                  ) : (
+                    <Typography sx={{ color: '#000000' }}>Carregando vídeo...</Typography>
+                  )}
+                </div>
               )}
-            </div>
-          )}
 
-          <CardContent>
-            <Box sx={{ display: 'flex', alignItems: 'baseline', justifyContent: 'space-between' }}>
-              <Typography gutterBottom variant="h5" component="div" sx={{ fontWeight: 'bold', color: '#000000' }}>
-                {project.name}
-              </Typography>
-              <Box sx={{ display: 'flex', alignItems: 'center' }}>
-                {user && isFavorite(project.id) ? (
-                  <>
-                    <FaStar
-                      onClick={() => handleFavoriteClick(project.id)}
-                      style={{ color: '#ff5722', fontSize: 20, marginRight: 4, cursor: 'pointer' }}
-                    />
-                    {project.totalFavorites > 0 && (
-                      <Typography variant="body2" sx={{ color: '#000', mr: 1 }}>
-                        {project.totalFavorites}
-                      </Typography>
+              <CardContent>
+                <Box sx={{ display: 'flex', alignItems: 'baseline', justifyContent: 'space-between' }}>
+                  <Typography gutterBottom variant="h5" component="div" sx={{ fontWeight: 'bold', color: '#000000' }}>
+                    {project.name}
+                  </Typography>
+                  <Box sx={{ display: 'flex', alignItems: 'center' }}>
+                    {user && isFavorite(project.id) ? (
+                      <>
+                        <FaStar
+                          onClick={() => handleFavoriteClick(project.id)}
+                          style={{ color: '#ff5722', fontSize: 20, marginRight: 4, cursor: 'pointer' }}
+                        />
+                        {project.totalFavorites > 0 && (
+                          <Typography variant="body2" sx={{ color: '#000', mr: 1 }}>
+                            {project.totalFavorites}
+                          </Typography>
+                        )}
+                      </>
+                    ) : (
+                      <>
+                        <FaRegStar
+                          onClick={() => handleFavoriteClick(project.id)}
+                          style={{ color: '#ff5722', fontSize: 20, marginRight: 4, cursor: 'pointer', }}
+                        />
+                        {project.totalFavorites > 0 && (
+                          <Typography variant="body2" sx={{ color: '#000', mr: 1 }}>
+                            {project.totalFavorites}
+                          </Typography>
+                        )}
+                      </>
                     )}
-                  </>
-                ) : (
-                  <>
-                    <FaRegStar
-                      onClick={() => handleFavoriteClick(project.id)}
-                      style={{ color: '#ff5722', fontSize: 20, marginRight: 4, cursor: 'pointer', }}
-                    />
-                    {project.totalFavorites > 0 && (
-                      <Typography variant="body2" sx={{ color: '#000', mr: 1 }}>
-                        {project.totalFavorites}
-                      </Typography>
-                    )}
-                  </>
-                )}
-                <IoChatbubbleEllipsesOutline onClick={handleCommentsClick} style={{ color: '#ff5722', fontSize: 20, cursor: 'pointer' }} />
-              </Box>
-            </Box>
-            <Typography variant="body1" sx={{ color: 'text.secondary', fontStyle: 'italic' }}>
-              {project.description}
-            </Typography>
-          </CardContent>
-          <CardActions sx={{ justifyContent: 'center' }}>
-            <Button size="large" variant="contained" sx={{ backgroundColor: '#000000', width: '180px' }} onClick={handleInfoClick} >
-              Saiba mais
-            </Button>
-            <Button size="large" variant="outlined" sx={{ borderColor: '#ff5722', color: '#ff5722', width: '180px' }} onClick={() => window.open('https://conciergevirtual.netlify.app/1', '_blank')}>
-              Ver Projeto
-            </Button>
-          </CardActions>
-        </Card>
-      ))}
-    </React.Fragment>
+                    <IoChatbubbleEllipsesOutline onClick={handleCommentsClick} style={{ color: '#ff5722', fontSize: 20, cursor: 'pointer' }} />
+                  </Box>
+                </Box>
+                <Typography variant="body1" sx={{ color: 'text.secondary', fontStyle: 'italic' }}>
+                  {project.description}
+                </Typography>
+              </CardContent>
+              <CardActions sx={{ justifyContent: 'center' }}>
+                <Button size="large" variant="contained" sx={{ backgroundColor: '#000000', width: '180px' }} onClick={handleInfoClick} >
+                  Saiba mais
+                </Button>
+                <Button size="large" variant="outlined" sx={{ borderColor: '#ff5722', color: '#ff5722', width: '180px' }} onClick={() => window.open('https://conciergevirtual.netlify.app/1', '_blank')}>
+                  Ver Projeto
+                </Button>
+              </CardActions>
+            </Card>
+          </Grid>
+        ))}
+      </Grid>
+    </Box>
   )
 }
--- src/components/cards/coments-about-project.jsx
+++ src/components/cards/coments-about-project.jsx
@@ -1,38 +1,108 @@
-import React from 'react'
-import { Box, Link } from '@mui/material'
+import React, { useState } from 'react'
+import { Box, Link, TextField, Button, IconButton } from '@mui/material'
 import Card from '@mui/material/Card'
 import CardContent from '@mui/material/CardContent'
 import Typography from '@mui/material/Typography'
 import HighlightOffIcon from '@mui/icons-material/HighlightOff'
-import { IoAddCircle } from "react-icons/io5"
+import { IoAddCircle, IoCheckmarkCircleSharp, IoCloseCircleSharp } from "react-icons/io5"
 import Tooltip from '@mui/material/Tooltip'
 
-
 export default function ComentsAboutProject({ handleCloseComments }) {
+    const [showCommentBox, setShowCommentBox] = useState(false)
+    const [comment, setComment] = useState('')
+
+    const handleAddComment = () => setShowCommentBox(true)
+    const handleCancelComment = () => {
+        setComment('')
+        setShowCommentBox(false)
+    }
+    const handleCommentChange = (e) => setComment(e.target.value)
+    const handleSaveComment = () => {
+        console.log('Comentário:', comment)
+        setComment('')
+        setShowCommentBox(false)
+    }
 
     return (
         <React.Fragment>
-            <Card sx={{ maxWidth: 400, border: '1px solid #ccc', borderRadius: 3, boxShadow: '0 8px 16px rgba(0, 0, 0, 0.2)', backgroundColor: '#FFFFFF', overflow: 'hidden', position: 'relative' }}>
+            <Card
+                sx={{
+                    maxWidth: 400,
+                    border: '1px solid #ccc',
+                    borderRadius: 3,
+                    boxShadow: '0 8px 16px rgba(0, 0, 0, 0.2)',
+                    backgroundColor: '#FFFFFF',
+                    overflow: 'hidden',
+                    position: 'relative',
+                }}
+            >
                 <Box sx={{ position: 'relative', height: 500, background: '#121212' }}>
-                    <HighlightOffIcon onClick={handleCloseComments} sx={{ position: 'absolute', top: 5, right: 5, fontSize: '1.2rem', cursor: 'pointer', color: '#ffffff', '&:hover': { color: '#ff5722' } }} />
+                    <HighlightOffIcon
+                        onClick={handleCloseComments}
+                        sx={{
+                            position: 'absolute',
+                            top: 5,
+                            right: 5,
+                            fontSize: '1.2rem',
+                            cursor: 'pointer',
+                            color: '#ffffff',
+                            '&:hover': { color: '#ff5722' },
+                        }}
+                    />
                     <CardContent>
                         <Typography variant="h6" sx={{ color: '#ffffff', textAlign: 'center', fontWeight: 'bold' }}>
                             Comentários do Projeto
                         </Typography>
-                        <Box sx={{ mt: 2, color: '#ffffff' }}>
-                            <Typography sx={{ fontSize: '14px', mb: 2 }}>
-                                Adicione seus comentários aqui:
-                            </Typography>
-                        </Box>
+                        {showCommentBox && (
+                            <>
+                                <TextField
+                                    value={comment}
+                                    onChange={handleCommentChange}
+                                    multiline
+                                    rows={3}
+                                    placeholder="Escreva seu comentário aqui..."
+                                    fullWidth
+                                    variant="outlined"
+                                    sx={{ mb: 2, background: '#ffffff', borderColor: '#ff5722' }}
+                                />
+                                <Box sx={{ display: 'flex', justifyContent: 'flex-end', gap: 2 }}>
+                                    <Typography onClick={handleSaveComment} variant="contained" sx={{ color: '#ffffff', cursor: 'pointer', '&:hover': { color: '#e64a19' }, display: 'flex', alignItems: 'center' }}>
+                                        <IoCheckmarkCircleSharp style={{ marginRight: 8 }} />
+                                        Salvar
+                                    </Typography>
+                                    <Typography onClick={handleSaveComment} variant="contained" sx={{ color: '#ffffff', cursor: 'pointer', '&:hover': { color: '#e64a19' }, display: 'flex', alignItems: 'center' }}>
+                                        <IoCloseCircleSharp style={{ marginRight: 8 }} />
+                                        Cancelar
+                                    </Typography>
+                                </Box>
+                            </>
+                        )}
                     </CardContent>
-                    <Tooltip title="Adicionar Comentário" arrow>
-                        <Link target="_blank" underline="none" rel="noopener noreferrer" sx={{ display: 'flex', alignItems: 'center', position: 'absolute', bottom: 5, right: 5, cursor: 'pointer', color: '#ffffff', fontWeight: 'bold', transition: 'color 0.3s ease', '&:hover ': { color: '#ff5722' }, '& svg': { marginRight: 1, transition: 'color 0.3s ease' } }}>
-                            <IoAddCircle fontSize={32} />
-                        </Link>
-                    </Tooltip>
+                    {!showCommentBox && (
+                        <Tooltip title="Adicionar Comentário" arrow>
+                            <Link
+                                onClick={handleAddComment}
+                                underline="none"
+                                sx={{
+                                    display: 'flex',
+                                    alignItems: 'center',
+                                    position: 'absolute',
+                                    bottom: 5,
+                                    right: 5,
+                                    cursor: 'pointer',
+                                    color: '#ffffff',
+                                    fontWeight: 'bold',
+                                    transition: 'color 0.3s ease',
+                                    '&:hover': { color: '#ff5722' },
+                                    '& svg': { marginRight: 1, transition: 'color 0.3s ease' },
+                                }}
+                            >
+                                <IoAddCircle fontSize={32} />
+                            </Link>
+                        </Tooltip>
+                    )}
                 </Box>
             </Card>
-
         </React.Fragment>
     )
 }
